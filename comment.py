2b424d88a8d6cff2f6dd458fcdb77dc3afc13ff8
omon212 <omonnuloraimkulov@gmail.com>
2023-12-09 23:14:46 +0500

    sort all code another file

---
+++ comment.py
@@ -0,0 +1,98 @@
+from main import dp, bot, son, Shogirdchalar, user_instagram, API_TOKEN
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.dispatcher import FSMContext
+from main import true_false, follow_button
+from aiogram import types
+from instagpy import InstaGPy
+
+
+
+@dp.message_handler(state=Shogirdchalar.Instagram_state, text="Comments💬")
+async def comments(message: Message, state: FSMContext):
+    await message.answer("Url kiriting")
+    await state.finish()
+    await Shogirdchalar.comment_state.set()
+
+
+@dp.message_handler(state=Shogirdchalar.comment_state, content_types=types.ContentType.TEXT)
+async def comments(message: Message, state: FSMContext):
+    url = message.text
+    user_instagram[str(message.from_user.id)] = url
+    if url.startswith("https://www.instagram.com"):
+        link = "https://lh3.googleusercontent.com/eZ95Z-mlPgDczM1CgYwafA6IYAmbD1FPhr8BXOBxEEzB7h5nfPWOeyKqCjABSpMuLVyHnLxRHd_NJQfrUvOTgnuBbalMOYzY88J96uQ7GJspdK1f7od-VCQHe2bw3-Kgi4OvkaY"
+        await message.answer_photo(link, caption="Kamentariya sonini tanlang", reply_markup=comment_button)
+        await state.finish()
+    else:
+        await message.answer("Mol faxim")
+
+
+@dp.callback_query_handler(text='comment-')
+async def minus_comment(call: types.CallbackQuery, state: FSMContext):
+    async def update_snecks_minus_comment_button1(chat_id, message_id, new_son):
+        comment_button = InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    InlineKeyboardButton(text='-1000 💬', callback_data='comment-'),
+                    InlineKeyboardButton(text=f'{son[call.message.chat.id]}', callback_data='comment_true'),
+                    InlineKeyboardButton(text='+1000 💬', callback_data='comment+'),
+
+                ],
+                [
+                    InlineKeyboardButton(text='Tasdiqlash✅', callback_data='comment_tasdiqlash')
+                ]
+            ],
+            resize_keyboard=True,
+        )
+
+        await bot.edit_message_reply_markup(chat_id=chat_id, message_id=message_id, reply_markup=comment_button)
+
+    global son
+    print(True)
+    if son[call.message.chat.id] <= 0:
+        await call.answer('Eng kam miqdor 1000 ta')
+    else:
+
+        son[call.message.chat.id] -= 1000
+        fake_son = son[call.message.chat.id]
+        print(fake_son)
+        if fake_son >= 0:
+            await update_snecks_minus_comment_button1(call.message.chat.id, call.message.message_id, fake_son)
+        else:
+            await call.answer('Eng kam miqdor 1000 ta')
+
+#
+@dp.callback_query_handler(text='comment+')
+async def plus_comment(call: types.CallbackQuery):
+    async def update_snecks_minus_comment_button1(chat_id, message_id, new_son):
+        comment_button = InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    InlineKeyboardButton(text='-1000 💬', callback_data='comment-'),
+                    InlineKeyboardButton(text=f'{son[call.message.chat.id]}', callback_data='comment_true'),
+                    InlineKeyboardButton(text='+1000 💬', callback_data='comment+'),
+
+                ],
+                [
+                    InlineKeyboardButton(text='Tasdiqlash✅', callback_data='comment_tasdiqlash')
+                ]
+            ],
+            resize_keyboard=True,
+        )
+
+        await bot.edit_message_reply_markup(chat_id=chat_id, message_id=message_id, reply_markup=comment_button)
+
+    global son
+    son[call.message.chat.id] += 1000
+    fake_son = son[call.message.chat.id]
+    print(fake_son)
+    if fake_son >= 0:
+        await update_snecks_minus_comment_button1(call.message.chat.id, call.message.message_id, fake_son)
+    else:
+        await call.answer('Eng kam miqdor 1000 ta')
+
+@dp.callback_query_handler(text='comment_tasdiqlash')
+async def tasdiq_comment(call: types.CallbackQuery):
+    instagram_nomi = user_instagram[f"{call.message.chat.id}"]
+    await bot.send_message(6596589901,
+                           f"Yangi Zakaz keldi\nBuyurtmachi: {call.from_user.username}\nTanlov Turi: Comment\nSoni: {son[call.message.chat.id]}\nInstagram:{instagram_nomi}")
+    await call.message.answer("To`lov turini tanlang!")
