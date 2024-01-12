8c91325f789549964bae35b02edee352702b2f6b
omon212 <omonnuloraimkulov@gmail.com>
2024-01-12 07:17:19 +0500

    fix buttons

--- keyboards/defaults/instagram.py
+++ keyboards/defaults/instagram.py
@@ -4,12 +4,12 @@ from aiogram.types import ReplyKeyboardMarkup,KeyboardButton
 instagram_paket = ReplyKeyboardMarkup(
     keyboard=[
         [
-            KeyboardButton(text="Followers👤"),
-            KeyboardButton(text="Likes❤️")
+            KeyboardButton(text="Followers 👤"),
+            KeyboardButton(text="Likes ❤️")
         ],
         [
-            KeyboardButton(text="Views👁️"),
-            KeyboardButton(text="Comments💬")
+            KeyboardButton(text="Views 👁️"),
+            KeyboardButton(text="Comments 💬")
         ]
     ],
     resize_keyboard=True,
