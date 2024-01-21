87ce992e7d30ce6e1ec3b8d534dad0ee9dd5f769
Omonullo Raimkulov <130729018+omon212@users.noreply.github.com>
2024-01-21 21:50:29 +0500

    Update comment.py

--- comment.py
+++ comment.py
@@ -106,7 +106,7 @@ Username Telegrami : <a href="https://t.me/{call.message.chat.username}">@{call.
     await call.message.answer(f'''
 Commentlar soni : <b>{son[call.message.chat.id]}</b> 💬
 Narxi : {son[call.message.chat.id] * 5} so'm 💰
-To'lov karta raqami 💳 : <code>8600092990835856</code>
+To'lov karta raqami 💳 : <code>5614682123000103</code>
 
 To'lovni qilgandan so'ng checkni adminga yuboring.
 Yo'qsa buyurtmangiz amalga oshirilmaydi !
